1d3afcfb48a512a051cd69ed98f4670073f65d3b
Dan Malec <dan.malec@gmail.com>
2022-02-05 12:44:31 -0500

    Removed game_model

--- WordleGS/wgs_game_engine.c
+++ WordleGS/wgs_game_engine.c
@@ -41,6 +41,7 @@ static char wgs_game_engine_secret_word[6] = "ROBOT";
 /* Local Prototypes */
 
 void GameEngine_NewSecretWord(char *word);
+int GameEngine_IndexOfLetter(char* word, char letter);
 
 
 /* Lifecycle Methods */
@@ -98,12 +99,86 @@ wgs_game_state GameEngine_GetGameState(void) {
   return wgs_game_engine_game_state;
 }
 
-void GameEngine_SetGameState(wgs_game_state game_state) {
-  wgs_game_engine_game_state = game_state;
-}
-
 BOOLEAN GameEngine_IsGameInProgress(void) {
   return wgs_game_engine_game_state == InProgress && (GuessState_GetRow() > 0 || GuessState_GetCol() > 0);
+}
+
+
+wgs_guess_status GameEngine_GuessCurrentWord(void) {
+  int current_guess_row = GuessState_GetRow();
+  int i, letter_index;
+  int matches = 0;
+  char tmp_secret[5];
+  char guess_word[] = "     ";
+  char c;
+
+  if (GuessState_GetRow() >= 6) return MaxGuesses;
+  if (GuessState_GetCol() < 5) return WordFilled;
+
+  GuessState_GetGuessWord(guess_word);
+  if (!Dictionary_IsValidGuess(guess_word)) return InvalidWord;
+
+  for (i=0; i<5; i++) {
+    wgs_letter_state letter_state = GuessState_GetLetterState(current_guess_row, i);
+
+    // Mark matches in advance - this will help with properly
+    // marking misplaced versus incorrect letters later.
+    if (letter_state.letter == wgs_game_engine_secret_word[i]) {
+      tmp_secret[i] = '+';
+    } else {
+      tmp_secret[i] = wgs_game_engine_secret_word[i];
+    }
+  }
+
+  for (i=0; i<5; i++) {
+    wgs_letter_state letter_state = GuessState_GetLetterState(current_guess_row, i);
+
+    c = letter_state.letter;
+    letter_index = c - 'A';
+
+    if (tmp_secret[i] == '+') {
+      GuessState_MaybeUpdateLetterStatus(i, gtCorrectLetter);
+      AlphabetState_MaybeUpdateLetterStatus(c, gtCorrectLetter);
+      matches++;
+    } else {
+      int letter_idx = GameEngine_IndexOfLetter(tmp_secret, c);
+
+      if (letter_idx >= 0) {
+        GuessState_MaybeUpdateLetterStatus(i, gtWrongPlaceLetter);
+        AlphabetState_MaybeUpdateLetterStatus(c, gtWrongPlaceLetter);
+
+        // Clear the letter, so it won't be double counted
+        // if it appears again in the guess.
+        tmp_secret[letter_idx] = ' ';
+      } else {
+        GuessState_MaybeUpdateLetterStatus(i, gtIncorrectLetter);
+        AlphabetState_MaybeUpdateLetterStatus(c, gtIncorrectLetter);
+      }
+    }
+  }
+
+  if (matches >= 5) {
+    wgs_game_engine_game_state = Won;
+    wgs_game_engine_win_stats[current_guess_row]++;
+  } else if (current_guess_row >= 5) {
+    wgs_game_engine_game_state = Lost;
+  } else {
+    GuessState_NextGuess();
+  }
+
+  return ValidGuess;
+}
+
+int GameEngine_IndexOfLetter(char* word, char letter) {
+  int i;
+
+  for (i=0; i<5; i++) {
+    if (word[i] == letter) {
+      return i;
+    }
+  }
+
+  return -1;
 }
 
 
@@ -115,10 +190,6 @@ void GameEngine_GetSecretWord(char *word) {
   }
 }
 
-
-void GameEngine_IncrementWinStat(int guess_num) {
-  wgs_game_engine_win_stats[guess_num]++;
-}
 
 int GameEngine_GetWinStat(int guess_num) {
   return wgs_game_engine_win_stats[guess_num];
